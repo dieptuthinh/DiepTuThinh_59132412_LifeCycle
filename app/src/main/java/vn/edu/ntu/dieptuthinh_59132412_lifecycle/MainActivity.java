7b1bf871f899fd49d7b12d00925920e0df046cd3
dieptuthinh <55689660+dieptuthinh@users.noreply.github.com>
2020-04-16 20:54:59 +0700

    Initial commit

---
+++ app/src/main/java/vn/edu/ntu/dieptuthinh_59132412_lifecycle/MainActivity.java
@@ -0,0 +1,64 @@
+package vn.edu.ntu.dieptuthinh_59132412_lifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity
+{
+    TextView txtTG;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("LifeCycle", "onCreate callded");
+        txtTG = findViewById(R.id.txtTG);
+        SimpleDateFormat spf = new SimpleDateFormat("HH:mm:ss");
+        String strDate = spf.format(new Date());
+        txtTG.setText(strDate);
+
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.d("LifeCycle", "onResume callded");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("LifeCycle", "onStart callded");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("LifeCycle", "onStop callded");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("LifeCycle", "onCreate callded");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("LifeCycle", "onDestroy callded");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("LifeCycle", "onRestart callded");
+    }
+}
